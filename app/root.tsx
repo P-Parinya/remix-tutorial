301f2da50a01016e116fc838519f068db07bdc52
P-Parinya <ppinggii@gmail.com>
2023-11-03 01:35:31 +0700

    chore: finished tutorial.

--- app/root.tsx
+++ app/root.tsx
@@ -1,13 +1,44 @@
+import { json, redirect, type LinksFunction, type LoaderFunctionArgs } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
+  NavLink,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
 } from "@remix-run/react";
 
+import appStyleHref from './app.css'
+import { createEmptyContact, getContacts } from "./data";
+export const links:LinksFunction = () => [{rel:"stylesheet",href:appStyleHref}]
+
+export const loader = async({request}:LoaderFunctionArgs) => {
+   const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q)
+  return json({contacts,q})
+}
+
+export const action = async()=>{
+  const contact = await createEmptyContact()
+  return redirect(`/contacts/${contact.id}/edit`)
+}
+
 export default function App() {
+  const {contacts,q} = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
+  const submit = useSubmit()
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
   return (
     <html lang="en">
       <head>
@@ -20,15 +51,22 @@ export default function App() {
         <div id="sidebar">
           <h1>Remix Contacts</h1>
           <div>
-            <Form id="search-form" role="search">
+            <Form id="search-form" 
+                onChange={(e)=>{ 
+                  const isFirstSearch = q === null;
+                submit(e.currentTarget, {
+                  replace: !isFirstSearch,
+                });}} role="search">
               <input
                 id="q"
+                defaultValue={q||""}
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                className={searching ? "loading" : ""}
               />
-              <div id="search-spinner" aria-hidden hidden={true} />
+              <div id="search-spinner" hidden={!searching} aria-hidden />
             </Form>
             <Form method="post">
               <button type="submit">New</button>
@@ -36,14 +74,46 @@ export default function App() {
           </div>
           <nav>
             <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
+              {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                  className={({ isActive, isPending }) =>
+                    isActive
+                      ? "active"
+                      : isPending
+                      ? "pending"
+                      : ""
+                  }
+                  to={`contacts/${contact.id}`}
+                >
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
             </ul>
           </nav>
+        </div>
+        <div id="detail" className={navigation.state ==="loading" && !searching ? "loading":""}>
+          <Outlet/>
         </div>
 
         <ScrollRestoration />
